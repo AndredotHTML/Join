e4bbb153c2abb0fac6d0cc27dec7fba5c4fb96fc
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-03-12 09:26:05 +0100

    update radio-btn style + function

--- script/addTask.js
+++ script/addTask.js
@@ -10,3 +10,37 @@ document.querySelectorAll("form select").forEach(select => {
         select.classList.remove("open");
     });
 });
+
+
+
+function radioBtnChecked(priority) {
+    let labelList = document.querySelectorAll(".radio-btn")
+    let priorityRef = priority
+    let inputRef = document.getElementById(priorityRef + "-rad")
+    let labelRef = document.querySelector('label[for="'+priorityRef+'-rad"]')
+    labelList.forEach(radioBtn => {
+        radioBtn.style.backgroundColor= `var(--secondaryColor)`;
+        radioBtn.style.color= `black`;
+    });
+    if (inputRef.checked) {
+        labelRef.style.backgroundColor= `var(--${priorityRef}Color)`;
+        labelRef.style.color= `var(--secondaryColor)`;
+    } 
+}
+
+
+
+// date form problem.
+// const placeholderArr = ["d","d","/","m","m","/","y","y","y","y"]
+
+
+
+// function placeholderDate() {
+//     let placeholderDateRef = document.getElementById("dateInput-add-task").value;
+//     let placeholderText = []
+//     for (let iPlaceholder = 0; iPlaceholder < placeholderArr.length; iPlaceholder++) {
+//         placeholderDisplay += placeholderArr[iPlaceholder]; 
+//     }
+//     console.log(placeholderDisplay);
+//     placeholderDateRef.value = placeholderDisplay
+// }
