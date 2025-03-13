7487fa8adb6ef2326cffe71af5cf872175ae5cff
André Zelmer <lordandre.skyguy@gmail.com>
2025-03-13 15:30:25 +0100

    login api implement and JSdocs for Login/Signup

--- script/login.js
+++ script/login.js
@@ -1,17 +1,7 @@
-function togglePassword() {
-    const passwordInput = document.getElementById("password");
-    const passwordIcon = document.querySelector(".input_icon_password");
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-      passwordIcon.src = "/assets/icons/visibility.png"
-    } else {
-      passwordInput.type = "password";
-      passwordIcon.src = "/assets/icons/visibility_off.png"
-    }
-  }
-
-
-function validateForm() {
+/**
+ * checks if the inputs in the form are valid and displays error messages if they are not 
+ */
+function validateLoginForm() {
     let email = document.getElementById("email");
     let password = document.getElementById("password");
     let errorMsg = document.getElementById("error_msg");
@@ -23,19 +13,45 @@ function validateForm() {
         email.classList.remove("error"); 
         password.classList.remove("error");
         errorMsg.style.display = "none";
-    }
+      }
 }
 
 
-async function login() {
-    
+/**
+ * redirects the user to the summary page 
+ * @param {*} User - userdata to give over information as parameter
+ */
+function redirectToSummary(User) {
+    window.location.href = "http://127.0.0.1:5500/html/summary.html"
 }
 
 
-function redirectToSummary() {
-    
+/**
+ * checks if the users e-mail and password match to login
+ */
+function login() {
+  const emailInput = document.getElementById("email").value;
+  const passwordInput = document.getElementById("password").value;
+  const user = users.find(user => user.userData.email === emailInput);
+  if (user) {
+      if (user.userData.password === passwordInput) {
+          handleSuccessfulLogin(user.userData);
+      } else {
+        email.classList.add("error"); 
+        password.classList.add("error"); 
+        errorMsg.style.display = "block";
+      }
+  } 
 }
 
+/**
+ * when login is succesful redirect the user to the summary
+ * @param {*} userData - userdata to give over information as parameter
+ */
+function handleSuccessfulLogin(userData) {
+  console.log("Angemeldeter Nutzer:", userData);
+  redirectToSummary(userData)
+}
 
 
 
