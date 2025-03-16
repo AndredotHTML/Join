c51aa7c8310b672069bc8f998fefb6b050bcdb3a
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-03-16 11:38:24 +0100

    custom drop-down menu and get contacts

--- script/addTask.js
+++ script/addTask.js
@@ -1,5 +1,11 @@
 const BASE_URL = "https://join-5677e-default-rtdb.europe-west1.firebasedatabase.app/"
 
+document.addEventListener("DOMContentLoaded", function () {
+    dropDownForCategory();
+    getUser(path = "/users");
+    dropDownForAssigned();
+});
+
 document.querySelectorAll("form select").forEach(select => {
     select.addEventListener("click", () => {
         if (select.classList.contains("open")) {
@@ -12,8 +18,6 @@ document.querySelectorAll("form select").forEach(select => {
         select.classList.remove("open");
     });
 });
-
-
 
 function radioBtnChecked(priority) {
     let labelList = document.querySelectorAll(".radio-btn")
@@ -65,10 +69,10 @@ function creatTask() {
             priority: priorityNewTaskRef.value,
             category: selectedOption.textContent,
             assigned: assignedUserRef,
+            status: "todo"
         }
     };
     postTask("/tasks", data)
-
 }
 
 async function postTask(path = "", data = {}) {
@@ -91,6 +95,63 @@ async function postTask(path = "", data = {}) {
         return null;
     }
 }
+
+async function getUser(path = "") {
+    try {
+        const response = await fetch(BASE_URL + path + ".json")
+        if (!response.ok) {
+            throw new Error("Fehler beim Empfangen der User");
+        }
+        const result = await response.json()
+        const userArray = result ? Object.values(result) : []
+        displayUser(userArray)
+        return result;
+    } catch (error) {
+        console.error("Fehler:", error.message);
+    }
+}
+
+function displayUser(userArray) {
+    let assignedToAreaRef = document.getElementById("assigned-to-display")
+    let allContacts = []
+    for (let i = 0; i < userArray.length; i++) {
+        let user = userArray[i];
+        let userName = user.name;
+        let userEmail = user.email;
+        allContacts += templateAssignedTo(userName)
+    }
+    assignedToAreaRef.innerHTML = allContacts
+}
+
+function dropDownForAssigned(){
+    console.log("function läuft");
+    let assignedInputRef = document.getElementById("assigned-to-input")
+    let assignedRef = document.getElementById("assigned-to-display")
+    let options = document.querySelectorAll(".assigned-to-container .assigned-contacts")
+    assignedInputRef.addEventListener("click", function () {
+        assignedRef.classList.toggle("d_none")
+    });
+}
+
+function dropDownForCategory() {
+    let categoryInputRef = document.getElementById("category-add-task")
+    let options = document.querySelectorAll(".wrapper-category .option-category")
+    categoryInputRef.addEventListener("click", function () {
+        options.forEach(option => {
+            option.classList.toggle("d_none")
+        });
+    });
+    options.forEach(option => {
+        option.addEventListener("click", function () {
+            categoryInputRef.textContent = this.textContent;
+            options.forEach(option => {
+                option.classList.add("d_none")
+            })
+        })
+    })
+}
+
+
 
 
 
--- script/template.js
+++ script/template.js
@@ -4,3 +4,15 @@ function subtaskTemplat(inputSubtaskVal) {
             </li>
     `
 }
+
+function templateAssignedTo(userName) {
+    return`
+            <div class="assigned-contacts d_flex">
+                <div class="name-icon">${userName.split(' ')[0][0]}
+                </div>
+                <div>${userName}</div>
+                <div class="checkbox"></div>
+            </div>
+    `
+}
+
