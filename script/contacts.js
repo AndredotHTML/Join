199129d0e27b1e26dbfbfe3cd0c2eaab2fdb9181
Stefan Krischan <stefan.krischan@live.at>
2025-03-16 18:56:10 +0100

    add functon to select first contact

---
+++ script/contacts.js
@@ -0,0 +1,13 @@
+/**
+ * selects element using id
+ * @param {string} id - HTML-Element Id
+ */
+function selectContact ( id ) {
+    const selectedElement = document.getElementById( id );
+    const alreadySelected = selectedElement.classList.contains( 'selected' );
+    document.querySelectorAll( '.contact' ).forEach( el => el.classList.remove( 'selected' ) );
+
+    if ( !alreadySelected ) {
+        selectedElement.classList.add( 'selected' );
+    }
+}
