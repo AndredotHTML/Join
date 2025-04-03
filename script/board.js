d44fa93bc210ff069d2a47644965d170473bef8d
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2025-04-03 18:55:27 +0200

    Sorting code

--- script/board.js
+++ script/board.js
@@ -3,10 +3,34 @@ let tasks = [];
 let currentDraggedElement;
 const predefinedColors = ["#FF5733", "#33FF57", "#3357FF", "#FF33A1", "#A133FF"];
 
+async function getCurrentUser() {
+    let userData = JSON.parse(localStorage.getItem('user'));
+    if (userData) {
+        user.push(userData);
+        generateUserIcon();
+    } else {
+        console.log("Kein Nutzer gefunden.");
+    }
+}
+
+function generateUserIcon() {
+    let userName = user[0].name;
+    let iconContainer = document.getElementById('icon-container');
+    let iconWrapper = document.getElementById('icon-wrapper');
+    if (userName) {
+        let initials = userName.split(' ')
+                                 .map(word => word.charAt(0).toUpperCase())
+                                 .slice(0, 2)
+                                 .join('');
+        iconContainer.textContent = initials;
+        iconWrapper.style.display = 'flex'; 
+    }
+}
 
 async function updateHTML() {
     await pushToUsersArray(); 
     await pushToTask();
+    await getCurrentUser();
     displayToDo();
     displayInProgress();
     displayAwaitFeedback();
