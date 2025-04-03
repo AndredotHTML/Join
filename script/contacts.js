fbcfbef2013fc9ec2a378a14abf15413bee1e30e
Stefan Krischan <stefan.krischan@live.at>
2025-04-03 12:04:53 +0200

    add color variables and improved header all sites

--- script/contacts.js
+++ script/contacts.js
@@ -1,33 +1,3 @@
-// contacts.js
-
-user = []
-
-
-async function getCurrentUser() {
-    let userData = JSON.parse(localStorage.getItem('user'));
-    if (userData) {
-        user.push(userData);
-        generateUserIcon();
-    } else {
-        console.log("Kein Nutzer gefunden.");
-    }
-}
-
-
-function generateUserIcon() {
-    let userName = user[0].name;
-    let iconContainer = document.getElementById('icon-container');
-    let iconWrapper = document.getElementById('icon-wrapper');
-    if (userName) {
-        let initials = userName.split(' ')
-                                 .map(word => word.charAt(0).toUpperCase())
-                                 .slice(0, 2)
-                                 .join('');
-        iconContainer.textContent = initials;
-        iconWrapper.style.display = 'flex'; 
-    }
-}
-
 // Globale Deklarationen
 const DETAIL_ELEM = document.getElementById( 'detail' );
 const BASE_URL = "https://join-5677e-default-rtdb.europe-west1.firebasedatabase.app/";
--- script/header.js
+++ script/header.js
@@ -1,3 +1,29 @@
+user = [];
+
+async function getCurrentUser () {
+    let userData = JSON.parse( localStorage.getItem( 'user' ) );
+    if ( userData ) {
+        user.push( userData );
+        generateUserIcon();
+    } else {
+        console.log( "Kein Nutzer gefunden." );
+    }
+}
+
+function generateUserIcon () {
+    let userName = user[ 0 ].name;
+    let iconContainer = document.getElementById( 'icon-container' );
+    let iconWrapper = document.getElementById( 'icon-wrapper' );
+    if ( userName ) {
+        let initials = userName.split( ' ' )
+            .map( word => word.charAt( 0 ).toUpperCase() )
+            .slice( 0, 2 )
+            .join( '' );
+        iconContainer.textContent = initials;
+        iconWrapper.style.display = 'flex';
+    }
+}
+
 /**
  * Shows or hides the Profiles menu.
  */
@@ -5,3 +31,7 @@ function toggleProfileMenu () {
     const profileMenu = document.getElementById( "profile_menu" );
     profileMenu.classList.toggle( "open" );
 }
+
+document.addEventListener( "DOMContentLoaded", function () {
+    getCurrentUser();
+} );
