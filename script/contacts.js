d274809f2fb885d3b253d2646f9626e546cdcc81
Stefan Krischan <stefan.krischan@live.at>
2025-05-07 17:05:47 +0200

    Outsource login verification to authLogIn.js

--- script/contacts.js
+++ script/contacts.js
@@ -3,25 +3,6 @@ const DETAIL_ELEM = document.getElementById( "detail" );
 const BASE_URL = "https://join-5677e-default-rtdb.europe-west1.firebasedatabase.app/";
 let contacts = [];
 
-
-/**
- * checks whether user is logged in or not and redirects to login
- */
-function authLogIn () {
-    if ( localStorage.getItem( "isLoggedIn" ) !== "true" ) {
-        redirectToLogin();
-    }
-}
-
-
-/**
- * redirects user to login
- */
-function redirectToLogin () {
-    location.href = "index.html";
-}
-
-
 function findContactById ( contactId ) {
     return contacts.find( contact => contact.id === contactId );
 }
